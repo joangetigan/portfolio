93b322b0c9096d0c5f5e325f8a3e54d5bd3908c4
joan <joan.getigan@gmail.com>
2017-07-13 08:19:13 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Joan Getigan | Portfolio</title>
+
+	<!-- CSS, font styles -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="style.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway:300|Droid Sans" rel="stylesheet">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-1"></div>
+
+			<div class="col-md-2 name">
+				<div class="surname">getigan</div>
+				<div class="firstname">JOAN</div>
+			</div>
+
+			<div class="col-md-2 pic">
+				<img src="" class="img-thumbnail">
+			</div>
+			<div class="col-md-2 pic">
+				<img src="" class="img-thumbnail">
+			</div>
+			<div class="col-md-2 pic">
+				<img src="" class="img-thumbnail">
+			</div>
+			<div class="col-md-2 pic">
+				<img src="" class="img-thumbnail">
+			</div>
+
+			<div class="col-md-1"></div>
+		</div><br><br>
+
+		<div class="entries">
+			<div class="entry">
+				<div class="title big">2017</div>
+				<div class="subtitle">August</div>
+				<div class="body">
+					<p>
+						Finished Coding Bootcamp at Tuitt Inc.<br> 
+						Created 3 portfolio-worthy websites.<br>
+						<p class="skill">Laravel Framework.</p>
+						See <a href="">Capstone3</a> - MVC.
+					</p>
+				</div>
+			</div>
+
+			<div class="entry">
+				<div class="title">2017</div>
+				<div class="subtitle">June</div>
+				<div class="body">
+					<p>
+						Back-end training.<br> 
+						<p class="skill">Javascript, PHP, Database, MySQL<br> 
+						+ (Jquery, Ajax)</p>
+						See <a href="http://bookmusketeers.x10host.com/" target="block">Capstone2</a> - Dynamic website.<br><br>
+						<img src="web2.png" class="web">
+					</p>
+				</div>
+			</div>
+
+			<div class="entry">
+				<div class="title">2017</div>
+				<div class="subtitle">May</div>
+				<div class="body">
+					<p>
+						Started Bootcamp at Tuitt Inc.<br>
+						Front-end training.<br> 
+						<p class="skill">HTML, CSS, Bootstrap</p>
+						See <a href="">Capstone1</a> - Static website.<br><br>
+						<img src="web1.png" class="web">
+					</p>
+				</div>
+			</div>
+
+			<div class="entry">
+				<div class="title">2017</div>
+				<div class="subtitle">January</div>
+				<div class="body">
+					<p>
+						Resigned from Caspo Incorporated as Sports Analyst.
+					</p>
+				</div>
+			</div>
+
+			<div class="entry">
+				<div class="title big">2014</div>
+				<div class="subtitle">May</div>
+				<div class="body">
+					<p>
+						Graduated from UP Diliman.<br>
+						<p class="skill">Bachelor of Science in Mathematics.</p>
+						Started working as Sports Analyst in Caspo Incorporated.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
